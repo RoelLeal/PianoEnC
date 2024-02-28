57d647913732acb589ff00f0caf7c107bb79e2ef
AlexisA303 <137253749+AlexisA303@users.noreply.github.com>
2024-02-28 13:05:05 -0600

    Letras en proceso

--- pianoenc.c
+++ pianoenc.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <windows.h>
+#include <conio.h>
 //DO
 #define let_a 97
 //RE
@@ -21,18 +22,78 @@
 // CERRAR PROGRAMA
 #define let_esc 27
 
+void gotoxy(int x,int y){  
+      HANDLE hcon;  
+      hcon = GetStdHandle(STD_OUTPUT_HANDLE);  
+      COORD dwPos;  
+      dwPos.X = x;  
+      dwPos.Y= y;  
+      SetConsoleCursorPosition(hcon,dwPos); 
+}
+
+void R(int x,int y){
+    // Posiciona el cursor en la posición (x, y)
+    gotoxy(x, y);
+
+    // Imprime la letra 'R' en mayúscula con el código ASCII 219 sin ciclos
+    gotoxy(x, y); printf("%c", 219);
+    gotoxy(x, y+1); printf("%c%c", 219, 219);
+    gotoxy(x, y+2); printf("%c", 219);
+    gotoxy(x, y+3); printf("%c", 219);
+    gotoxy(x, y+4); printf("%c", 219);
+    gotoxy(x, y+5); printf("%c", 219);
+
+    gotoxy(x+1, y+2); printf("%c", 219);
+    gotoxy(x+2, y+1); printf("%c", 219);
+    gotoxy(x+3, y+2); printf("%c", 219);
+    gotoxy(x+4, y); printf("%c", 219);
+    gotoxy(x+4, y+1); printf("%c", 219);
+    gotoxy(x+4, y+2); printf("%c", 219);
+    gotoxy(x+4, y+3); printf("%c", 219);
+    gotoxy(x+4, y+4); printf("%c", 219);
+}
 int main() {
 	char letra;
+	int i, j, y = 1;
 	while(1) {
 		letra = getch();
 		letra = tolower(letra);
 		switch(letra) {
 			case let_a:
 				// DO
+				system("cls");
+				system("color 1");
+      			for (i = 0; i < 5; i++) {
+        			gotoxy(50, 10 + i); 
+       				for (j = 0; j < 5; j++) {
+            			if ((i == 0 || i == 4) && (j > 0 && j < 4)) {
+              				  printf("%c", 219); 
+           				 } else if ((j == 1 || j == 4) && (i > 0 && i < 4)) {
+                			printf("%c", 219); 
+            				} else {
+               				 printf(" "); 
+          					  }
+       					 }
+   				 }
+   				 
+   				 
+   				for (i = 0; i < 5; i++) {
+        			gotoxy(60, 10 + i); // Posiciona el cursor en la coordenada (x, y+i)
+        			for (j = 0; j < 5; j++) {
+          				  if (i == 0 || i == 4 || j == 0 || j == 4) {
+              				  printf("%c", 219); // Imprime el caracter con código ASCII 219 (bloque sólido)
+            				} else {
+               				 printf(" "); // Imprime un espacio en blanco para el interior de la "O"
+           					 }
+        				}
+    			}
 				Beep(261.626, 500);
 				break;
 			case let_s:
 				// RE
+				system("cls");
+				system("color 2");
+				R(1,1);
 				Beep(293.66, 500);
 				break;
 			case let_d:
